352caad18b7f7159645719de81f945a2f174bcae
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-02-12 00:37:10 +0500

    header fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ export default function Footer() {
           {/* Logo Section */}
           <motion.div variants={fadeInVariants} className="space-y-4">
             <img
-              src="BellClinix-unscreen.gif"
+              src="footerlogo.gif"
               alt="bellclinix Logo"
               className="h-auto -ml-3 w-44"
             />
@@ -46,21 +46,6 @@ export default function Footer() {
           </motion.div>
 
           {/* Quick Links */}
-          {/* <motion.div variants={fadeInVariants} className="space-y-4">
-            <h3 className="text-lg font-bold text-gray-700">Quick Links</h3>
-            <ul className="space-y-2">
-              {["Who We Are", "What We Do", "Contact Us"].map((link, index) => (
-                <li key={index}>
-                  <Link
-                    to="#"
-                    className="text-white hover:underline hover:text-gray-300"
-                  >
-                    {link}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </motion.div> */}
           <motion.div variants={fadeInVariants} className="space-y-4">
             <h3 className="text-lg font-bold text-gray-700">Quick Links</h3>
             <ul className="space-y-2">
