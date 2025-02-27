2e0182e24d4a0f167f1c7f1b0ff5f847a1d987af
Muhammad Anas <41787190+Anaskhaan@users.noreply.github.com>
2025-02-27 20:27:24 +0500

    fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div>
       <div className="flex justify-center items-center">
-        <div className="w-full h-auto bg-black  p-24">
+        <div className="w-full h-auto bg-black  lg:p-24 xl:p-24 2xl:p-24 p-12">
           <h1 className="text-white lg:text-5xl text-xl text-center p-6">
             Contact Us
           </h1>
